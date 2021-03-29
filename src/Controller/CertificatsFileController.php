c12c02b4896f095067fa1dd3ee6e2c11ab65822b
Quaranta Arthur <arthur.quaranta@ynov.com>
2021-03-29 11:25:05 +0200

    Certificats is here

--- src/Controller/CertificatsFileController.php
+++ src/Controller/CertificatsFileController.php
@@ -6,16 +6,35 @@ use App\Entity\Certificats;
 use App\Entity\Information;
 use App\Form\CertificatsFileType;
 use App\Repository\InformationRepository;
+use DateTime;
 use Doctrine\Persistence\ObjectManager;
+use phpDocumentor\Reflection\PseudoTypes\True_;
+use phpDocumentor\Reflection\Types\Self_;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints\Date;
 
 class CertificatsFileController extends AbstractController
 {
+
+    /**
+     * Permet de convertir la date qu'on récupère d'un certificats
+     */
+    public static function conversionDate($date)
+    {
+        //On récupère le timestamp du fichier.
+        $d = \DateTime::createFromFormat('ymdHisP', $date);
+        $dateGMT = new \DateTime();
+        $dateGMT->setTimestamp($d->getTimestamp());
+
+        //Puis on le formate à une date MySQL.
+        return $dateGMT->format('Y-m-d H:i:s');
+    }
+
     /**
      * @Route("/user/list", name="app_user_list")
      */
@@ -38,22 +57,33 @@ class CertificatsFileController extends AbstractController
             
             /** @var UploadedFile $certificatsFile */
             $certificatsFile = $form->get('certificats')->getData();
-            $data = openssl_x509_parse($certificatsFile->getContent());
+            $data = openssl_x509_parse(openssl_x509_read($certificatsFile->getContent()));
+            $information = new Information();
 
-            $data = new Information();
+            $valideDate = DateTime::createFromFormat('ymdHisP', $data['validFrom']);
+            $valideGMT = new DateTime();
+            $valideGMT->setTimestamp($valideDate->getTimestamp());
+            $valideGMT->format('Y-m-d H:i:s');
 
-            $data
-                ->setSociety($$data->getSociety())
-                ->setDomain($data->getDomain())
-                ->setProviderSociety($data->getProviderSociety())
-                ->setProviderDomain($data->getProviderDomain())
-                ->setValideDate($data->getValideDate())
-                ->setExpireDate($data->getValideDate())
+            $valideExpire = DateTime::createFromFormat('ymdHisP', $data['validTo']);
+            $expireGMT = new DateTime();
+            $expireGMT->setTimestamp($expireGMT->getTimestamp());
+            $expireGMT->format('Y-m-d H:i:s');
+
+            $information
+                ->setSociety($data['subject']['O'])
+                ->setDomain($data['subject']['CN'])
+                ->setProviderSociety($data['issuer']['O'])
+                ->setProviderDomain($data['issuer']['CN'])
+                ->setValideDate($valideGMT)
+                ->setExpireDate($expireGMT)
             ;
 
-            $manager = new ObjectManager();
-            $manager->persist($data);
-            $manager->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($information);
+            $entityManager->flush();
+
+            //dd(Self::conversionDate($data['validFrom']));
 
             return $this->redirectToRoute('app_user_list');
         }
